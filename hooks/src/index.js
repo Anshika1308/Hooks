bc25154eef31087b88016c40f56361341d0fb7b4
ganshika <ganshika@bridgetree.com>
2022-02-16 16:05:28 +0530

    first commit

---
+++ hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import DigitalC from './DigitalC';
+
+
+
+ReactDOM.render(<> <App /> <DigitalC/> </>, document.getElementById('root'));
+
+
